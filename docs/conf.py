159a77714c0cb0dfd5f16ac47146d02e3963d3f0
Peter Slump <github@yarf.nl>
2018-02-24 23:59:23 +0100

    Bump version: 0.0.1 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Peter Slump'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
